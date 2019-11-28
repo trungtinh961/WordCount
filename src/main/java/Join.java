ce21108ce6b944522dbd3673d645c746c2c00d3a
trungtinh <nguyentrungtinh961@gmail.com>
2019-11-28 20:16:09 +0700

    bai2

--- src/main/java/Join.java
+++ src/main/java/Join.java
@@ -1,6 +1,5 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -12,27 +11,21 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: andrea
- * Date: 13/04/14
- * Time: 16.15
- */
-public class JoinWithSort {
+public class Join {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length != 2) {
-            System.err.println("Usage: JoinWithSort <in> <out>");
+            System.err.println("Usage: Join <in> <out>");
             System.exit(2);
         }
         Job job = Job.getInstance(conf);
-        job.setJobName("JoinWithSort");
-        job.setJarByClass(JoinWithSort.class);
+        job.setJobName("Join");
+        job.setJarByClass(Join.class);
 
-        job.setMapperClass(JoinWithSortMapper.class);
-        job.setReducerClass(JoinWithSortReducer.class);
+        job.setMapperClass(Mymapper.class);
+        job.setReducerClass(Myreducer.class);
 
         job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(Text.class);
@@ -43,37 +36,34 @@ public class JoinWithSort {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
-    public static class JoinWithSortMapper extends Mapper<Object, Text, Text, Text> {
+    public static class Mymapper extends Mapper<Object, Text, Text, Text> {
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-            // gets filename of the input file for this record
             FileSplit fileSplit = (FileSplit) context.getInputSplit();
             String filename = fileSplit.getPath().getName();
-            String[] fields = value.toString().split(("\t"));
+            String[] elements = value.toString().split(("\t"));
             if (filename.equals("forum_nodes_no_lf.tsv")) {
-                if (fields.length > 5) {
-                    // skips the header row
-                    if (!fields[0].equals("\"id\"")) {
-                        String authorId = fields[3].substring(1, fields[3].length() - 1);
-                        String score = fields[8].substring(1,fields[8].length() - 1);
+                if (elements.length > 5) {
+                    if (!elements[0].equals("\"id\"")) {
+                        String authorId = elements[3].substring(1, elements[3].length() - 1);
+                        String score = elements[8].substring(1, elements[8].length() - 1);
                         context.write(new Text(authorId), new Text("score" + score));
                     }
                 }
             }
             else {
-                // skips the header row
-                if (!fields[0].equals("\"user_ptr_id\"")) {
-                    String authorId = fields[0].substring(1, fields[0].length() - 1);
-                    String reputation = fields[1].substring(1, fields[1].length() - 1);
+                if (!elements[0].equals("\"user_ptr_id\"")) {
+                    String authorId = elements[0].substring(1, elements[0].length() - 1);
+                    String reputation = elements[1].substring(1, elements[1].length() - 1);
                     context.write(new Text(authorId), new Text("reput" + reputation));
                 }
             }
         }
     }
 
-    public static class JoinWithSortReducer extends Reducer<Text, Text, Text, Text> {
+    public static class Myreducer extends Reducer<Text, Text, Text, Text> {
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
